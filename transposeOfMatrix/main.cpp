a887dd23325ae4faae7fcac67aa72ada4f175d6f
Maria Velikova <maria.y.velikova@gmail.com>
2018-11-08 10:09:24 +0200

    1.Transpose of a matrix

---
+++ transposeOfMatrix/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //Въвеждаме броя на редовете и колоните
+    int rows,cols;
+    cin>>rows>>cols;
+
+    //Създаваме динамичен двумерен масив  и въвеждаме стойности в него
+    double ** matrix=new double*[rows];
+    for (int i = 0; i < rows; ++i) {
+        matrix[i]=new double[cols];
+        for (int j = 0; j < cols; ++j) {
+            cin>>matrix[i][j];
+        }
+
+    }
+    //Създаване на транспонентна матрица
+    double** trans=new double *[cols];
+    for(int i=0; i < cols; i++)
+    {
+        trans[i]=new double[rows];
+    }
+
+    //Попълване на новата транспонирана матрица
+    for (int l = 0; l < rows; ++l) {
+        for (int i = 0; i < cols; ++i) {
+            trans[i][l]=matrix[l][i];
+        }
+    }
+
+    for (int k = 0; k < cols; ++k) {
+        for (int i = 0; i <rows; ++i) {
+            cout<<trans[k][i]<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0; i < cols; i++)
+    {
+        delete[] trans[i];
+    }
+    delete[] trans;
+
+    for(int i=0; i < rows; i++)
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
